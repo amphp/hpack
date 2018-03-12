6678661c2f1178b6b6ae71beeabe10d74ea0091f
Niklas Keller <me@kelunik.com>
2018-03-12 08:49:27 +0100

    Change and fix code style

--- test/HPackTest.php
+++ test/HPackTest.php
@@ -22,22 +22,22 @@ class HPackTest extends TestCase {
 
     public function provideDecodeCases() {
         $root = __DIR__."/../vendor/http2jp/hpack-test-case";
-        $paths = glob("$root/*/*.json");
+        $paths = \glob("$root/*/*.json");
         foreach ($paths as $path) {
-            if (basename(dirname($path)) == "raw-data") {
+            if (\basename(\dirname($path)) == "raw-data") {
                 continue;
             }
 
-            $data = json_decode(file_get_contents($path));
+            $data = \json_decode(\file_get_contents($path));
             $cases = [];
             foreach ($data->cases as $case) {
                 foreach ($case->headers as &$header) {
                     $header = (array) $header;
-                    $header = [key($header), current($header)];
+                    $header = [\key($header), \current($header)];
                 }
-                $cases[$case->seqno] = [hex2bin($case->wire), $case->headers];
+                $cases[$case->seqno] = [\hex2bin($case->wire), $case->headers];
             }
-            yield basename($path).": $data->description" => [$cases];
+            yield \basename($path).": $data->description" => [$cases];
         }
     }
 
@@ -51,8 +51,8 @@ class HPackTest extends TestCase {
             $encoded = $hpack->encode($input);
             $hpack = new HPack;
             $decoded = $hpack->decode($encoded, self::MAX_LENGTH);
-            sort($output);
-            sort($decoded);
+            \sort($output);
+            \sort($decoded);
             $this->assertEquals($output, $decoded, "Failure on testcase #$i (standalone)");
         }
 
@@ -62,30 +62,30 @@ class HPackTest extends TestCase {
         foreach ($cases as $i => list($input, $output)) {
             $encoded = $encHpack->encode($input);
             $decoded = $decHpack->decode($encoded, self::MAX_LENGTH);
-            sort($output);
-            sort($decoded);
+            \sort($output);
+            \sort($decoded);
             $this->assertEquals($output, $decoded, "Failure on testcase #$i (shared context)");
         }
     }
 
     public function provideEncodeCases() {
         $root = __DIR__."/../vendor/http2jp/hpack-test-case";
-        $paths = glob("$root/raw-data/*.json");
+        $paths = \glob("$root/raw-data/*.json");
         foreach ($paths as $path) {
-            $data = json_decode(file_get_contents($path));
+            $data = \json_decode(\file_get_contents($path));
             $cases = [];
             $i = 0;
             foreach ($data->cases as $case) {
                 $headers = [];
                 foreach ($case->headers as &$header) {
                     $header = (array) $header;
-                    $header = [key($header), current($header)];
+                    $header = [\key($header), \current($header)];
                     $headers[$header[0]][] = $header[1];
                 }
                 $cases[$case->seqno ?? $i] = [$headers, $case->headers];
                 $i++;
             }
-            yield basename($path) . (isset($data->description) ? ": $data->description" : "") => [$cases];
+            yield \basename($path) . (isset($data->description) ? ": $data->description" : "") => [$cases];
         }
     }
 }
