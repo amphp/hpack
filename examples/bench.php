c84abdba280809d207761347f7f0c8178351e339
Niklas Keller <me@kelunik.com>
2019-12-11 22:17:44 +0100

    Implement hpack using nghttp2 via FFI if available

--- examples/bench.php
+++ examples/bench.php
@@ -24,19 +24,30 @@ foreach ($paths as $path) {
 }
 
 $minDuration = \PHP_INT_MAX;
+$minOps = \PHP_INT_MAX;
 
 for ($i = 0; $i < 10; $i++) {
     $start = \microtime(true);
+    $ops = 0;
 
     foreach ($tests as $test) {
-        $hpack = new Amp\Http\HPack;
-        foreach ($cases as list($input, $output)) {
-            $hpack->decode($input, 8192);
+        $hpack = new Amp\Http\Internal\HPackNative;
+        foreach ($cases as [$input, $output]) {
+            $headers = $hpack->decode($input, 4096);
+            $hpack->encode($headers);
+
+            if ($headers !== $output) {
+                print 'Invalid headers' . \PHP_EOL;
+                exit(1);
+            }
+
+            $ops++;
         }
     }
 
     $duration = \microtime(true) - $start;
     $minDuration = \min($minDuration, $duration);
+    $minOps = \min($ops, $minOps);
 }
 
-print "$minDuration s" . PHP_EOL . PHP_EOL;
+print "$minOps in $minDuration seconds" . \PHP_EOL;
