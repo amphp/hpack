02d79920a255e786bf360c79902e13a2fff2b408
Niklas Keller <me@kelunik.com>
2023-08-15 21:18:31 +0200

    Add php-fuzzer oracle

---
+++ tools/php-fuzzer/decode-oracle.php
@@ -0,0 +1,16 @@
+<?php
+
+require __DIR__ . '/../../vendor/autoload.php';
+
+use Amp\Http\Internal;
+
+$fuzzer->setTarget(function (string $input) {
+    $result1 = (new Internal\HPackNghttp2)->decode($input, 8192);
+    $result2 = (new Internal\HPackNative)->decode($input, 8192);
+
+    if ($result1 !== $result2) {
+        throw new \Error('Mismatch: ' . var_export($result1, true) . ' / ' . var_export($result2, true));
+    }
+});
+
+$fuzzer->setMaxLen(1024);
