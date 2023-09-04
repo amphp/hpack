22ddf96bc559c4f75754527c40e366ff18377c75
Niklas Keller <me@kelunik.com>
2023-09-04 20:52:29 +0200

    Ignore fuzzing results for now if either implementation returns null

--- tools/php-fuzzer/decode-oracle.php
+++ tools/php-fuzzer/decode-oracle.php
@@ -8,6 +8,10 @@ $fuzzer->setTarget(function (string $input) {
     $result1 = (new Internal\HPackNghttp2)->decode($input, 8192);
     $result2 = (new Internal\HPackNative)->decode($input, 8192);
 
+    if ($result1 === null || $result2 === null) {
+        return;
+    }
+
     if ($result1 !== $result2) {
         throw new \Error('Mismatch: ' . var_export($result1, true) . ' / ' . var_export($result2, true));
     }
